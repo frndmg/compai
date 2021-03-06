61c52737220aaf6f2fe2055857ccfe06003ef767
Fernando Martínez González <frndmartinezglez@gmail.com>
2021-03-06 01:57:36 +0100

    ci: configure github actions
    - add coverage reports

--- compai/__init__.py
+++ compai/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import version
 from .compai import (apply, compose, const, dict_map, ffilter, fmap, identity,
                      length, none_map, swap, tuple_map, tupled)
 
-__version__ = version('fp-py')
+__version__ = version('compai')
 
 __all__ = [
     'compose',
