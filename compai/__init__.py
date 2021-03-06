533366bc95990bd2eae767975043347a4c507eae
Fernando Martínez González <frndmartinezglez@gmail.com>
2021-03-06 12:39:46 +0100

    build: add support for more pthon versions

--- compai/__init__.py
+++ compai/__init__.py
@@ -1,7 +1,23 @@
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
 
-from .compai import (apply, compose, const, dict_map, ffilter, fmap, identity,
-                     length, none_map, swap, tuple_map, tupled)
+
+from .compai import (
+    apply,
+    compose,
+    const,
+    dict_map,
+    ffilter,
+    fmap,
+    identity,
+    length,
+    none_map,
+    swap,
+    tuple_map,
+    tupled,
+)
 
 __version__ = version('compai')
 
